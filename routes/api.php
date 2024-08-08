1b1605b69341f342662213d259c4fe4fe1bc4b77
Kei-K23 <keiksl2301@gmail.com>
2024-08-08 15:55:29 +0630

    Add custom json response middleware

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/auth/register', [RegisterController::class, 'register']);
+Route::post('/auth/login', [LoginController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('customers', CustomerController::class);
+});
